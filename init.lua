672cb0b82880c12846635d89c5418aa71f20245a
xvzftube <xvzf.tube@gmail.com>
2022-12-03 22:59:36 -0500

    add more detail

--- init.lua
+++ init.lua
@@ -5,6 +5,67 @@ vim.g.everforest_background = "soft"
 vim.g.everforest_better_performance = 1
 --vim.g.coc_node_path='/home/user/.nvm/versions/node/v19.2.0/bin/node'
 --vim.g.coc_npm_path='/home/user/.nvm/versions/node/v19.2.0/bin/npm'
+--
+--iron
+local iron = require("iron.core")
+local view = require("iron.view")
+
+iron.setup {
+  config = {
+    -- Whether a repl should be discarded or not
+    scratch_repl = true,
+    -- Your repl definitions come here
+    repl_definition = {
+      sh = {
+        -- Can be a table or a function that
+        -- returns a table (see below)
+        command = {"bash"}
+      }
+    },
+    -- How the repl window will be displayed
+    -- See below for more information
+    repl_open_cmd = require('iron.view').bottom(40),
+  },
+  -- Iron doesn't set keymaps by default anymore.
+  -- You can set them here or manually add keymaps to the functions in iron.core
+  keymaps = {
+    send_motion = "<space>sc",
+    visual_send = "<space>sc",
+    send_file = "<space>sf",
+    send_line = "<space>sl",
+    send_mark = "<space>sm",
+    mark_motion = "<space>mc",
+    mark_visual = "<space>mc",
+    remove_mark = "<space>md",
+    cr = "<space>s<cr>",
+    interrupt = "<space>s<space>",
+    exit = "<space>sq",
+    clear = "<space>cl",
+  },
+  -- If the highlight is on, you can change how it looks
+  -- For the available options, check nvim_set_hl
+  highlight = {
+    italic = true
+  },
+  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+  repl_open_cmd = view.top("10%")
+}
+
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+-------------------iron
+--
+--
+--coc
+-------------coc
+
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
 
 local ensure_packer = function()
   local fn = vim.fn
@@ -23,9 +84,8 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'sainnhe/everforest'
   use {'neoclide/coc.nvim', branch = 'release'}
-  use { 'quarto-dev/quarto-nvim',
-  requires = {'neovim/nvim-lspconfig'}
-}
+  use { 'quarto-dev/quarto-nvim',requires = {'neovim/nvim-lspconfig'}}
+  use {'hkupty/iron.nvim'}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
